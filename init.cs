23e6f5990d0e34b93174e4707cad835e5a2afeed
Henrique Selau de Oliveira <hso.softwares@gmail.com>
2023-10-12 20:40:14 -0300

    File mng

--- init.cs
+++ init.cs
@@ -6,8 +6,8 @@ public partial class init : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello World");
-
+	
+	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
